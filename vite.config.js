38e77d2bb6a39d195b82cb4b60a7aa173dd45b16
arundhi04101996@gmail.com <arundhi04101996@gmail.com>
2024-01-22 14:08:41 +0530

    http changed

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:8000",
+        target: "https://estate-api-44ea.onrender.com/",
         secure: false,
       },
     },
